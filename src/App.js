3c227d0fafbf4d8908b93a40d98defefcd5d2db3
Marko Lauronen <marko.lauronen@solidabis.com>
2022-01-26 16:31:37 +0200

    finalisointia

--- src/App.js
+++ src/App.js
@@ -1,8 +1,94 @@
+import { useState } from 'react';
+import { Form, Button, Card, ListGroup, Row, Col, Container } from 'react-bootstrap';
 import './App.css';
 
-function App() {
+function TodoItem({onChange, item, index}) {
+  return (        
+    <ListGroup.Item>
+      <Row>
+        <Col xs={1}>
+        <Form.Check onChange={onChange} 
+                  data-testid={`item-${index+1}-check`}/>
+        </Col>
+        <Col>
+          {item}
+        </Col>
+      </Row>
+    </ListGroup.Item>
+  );
+}
+
+function ItemAdder({itemAdded}) {
+  const [ input, setInput ] = useState('');
+
+  const inputChanged = (event) => {
+    setInput(event.target.value);
+  };
+
+  const addButtonClicked = () => {
+    itemAdded(input);
+    setInput('');
+  }
+
   return (
-    <div></div>
+    <Row>
+      <Col>
+        <Form.Control type="text" 
+                      onChange={inputChanged} 
+                      value={input} 
+                      placeholder="Enter description"
+                      data-testid='todo-input'/>
+      </Col>
+      <Col xs={2}>
+        <Button onClick={addButtonClicked} 
+                disabled={input.length===0}
+                aria-label="add-todo"
+                className="float-end">
+                  Add
+        </Button>
+      </Col>
+    </Row>
+  );
+}
+
+function App() {
+  const [ todoItems, setTodoItems ] = useState([]);
+  const [ itemsDone, setItemsDone ] = useState(0);
+
+  const itemChecked = (event) => {
+    setItemsDone(itemsDone+(event.target.checked ? 1 : -1));
+  };
+
+  const itemAdded = (input) => {
+    setTodoItems([...todoItems, input]);
+  };
+
+  return (
+    <Container>
+      <Card border="secondary" className="shadow">
+        <Card.Header as="h5">Todo-list</Card.Header>
+        <Card.Body>
+          <ListGroup className="mb-3" data-testid="todoitems">
+            {todoItems.map((item, index) => 
+              <TodoItem key={index}
+                        onChange={itemChecked} 
+                        item={item} 
+                        index={index}/>
+            )}
+          </ListGroup>
+          <ItemAdder itemAdded={itemAdded}/>
+        </Card.Body>
+        <Card.Footer className="text-center">
+          {todoItems.length===0 ?
+          (<>Nothing to do</>)
+          :
+          todoItems.length===itemsDone ? <>All done!</>
+          :
+          (<>{itemsDone}/{todoItems.length} done</>)
+          }
+        </Card.Footer>
+      </Card>
+    </Container>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,119 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Todo-list', () => {
+  function enterItemText(text) {
+    fireEvent.change(todoInput(), {target: {value: text}});
+  }
+
+  function clickToAddItem() {
+    fireEvent.click(addButton());
+  }
+
+  function todoInput() {
+    return screen.getByTestId('todo-input');
+  }
+
+  function addItem(text) {
+    enterItemText(text);
+    clickToAddItem();
+  }
+
+  function itemCheckbox(item) {
+    return screen.getByTestId(`item-${item}-check`);
+  }
+
+  function clickItem(item) {
+    fireEvent.click(itemCheckbox(item));
+  }
+
+  function addButton() {
+    return screen.getByRole('button', {name: 'add-todo'});
+  }
+
+
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it('shows it is a todo-list app', () => {
+    expect(screen.getByText('Todo-list')).toBeInTheDocument();
+  });
+  
+  it('has an input field for entering todo item', () => {
+    const input = todoInput();
+    expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute('placeholder', 'Enter description');
+  });
+
+  it('has a button for adding the todo item', () => {
+    const button = addButton();
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('Add');
+  });
+
+  it('has no todo items initially', () => {
+    const items = screen.getByTestId('todoitems');
+    expect(items).toBeEmptyDOMElement();
+  });
+
+  it('adds an item to list when text is entered and button clicked', () => {
+    addItem('my todo item');
+    expect(screen.getByText('my todo item')).toBeInTheDocument();
+  });
+
+  
+  it('should display checkboxes near the added item', () => {
+    addItem('my todo item');
+    addItem('todo item 2')
+    expect(itemCheckbox(1)).toBeInTheDocument();
+    expect(itemCheckbox(2)).toBeInTheDocument();
+  });
+
+  it('should display no items if no items are present', () => {
+    expect(screen.getByText('Nothing to do')).toBeInTheDocument();
+  });
+
+  it('should display the number of items not done', () => {
+    addItem('my todo item');
+    addItem('todo item 2');
+    expect(screen.getByText('0/2 done')).toBeInTheDocument();
+  });
+
+  it('should change the number of done items when item is checked', () => {
+    addItem('my todo item');
+    addItem('todo item 2');
+    clickItem(2);
+    expect(screen.getByText('1/2 done')).toBeInTheDocument();
+  });
+
+  it('should reduce the number of done items when item is unchecked', () => {
+    addItem('my todo item');
+    addItem('todo item 2');
+    clickItem(2);
+    clickItem(2);
+    expect(screen.getByText('0/2 done')).toBeInTheDocument();
+  });
+
+  it('should display all done if all items are checked', () => {
+    addItem('todo item 1');
+    addItem('todo item 2');
+    clickItem(1);
+    clickItem(2);
+    expect(screen.getByText('All done!')).toBeInTheDocument();
+  });
+
+  it('should show button as disabled if no item text has been entered', () => {
+    expect(addButton()).toBeDisabled();
+  })
+
+  it('should show button enabled when at least one character is entered', () => {
+    enterItemText('f');
+    expect(addButton()).not.toBeDisabled();
+  })
+
+  it('should clear the input text after adding', () => {
+    addItem('my todo item');
+    expect(todoInput()).toHaveValue('');
+  });
 });
